244b5eae160d3433873f1cd53b3485ebf70e9b51
Mario Garcia Pinan <jaitoone@gmail.com>
2019-08-09 10:17:55 +0100

    Added README

--- app.rb
+++ app.rb
@@ -1,19 +1,17 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
-  #post '/names' do
-  #  @player_1_name = params[:player_1_name]
-  #  @player_2_name = params[:player_2_name]
-  #  erb :play
-  #end
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
